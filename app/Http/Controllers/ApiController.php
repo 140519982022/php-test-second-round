17ba2169acf42ce7307855fe6611efe9b5f82e2d
Nikita Kose <nikitakose1414@gmail.com>
2023-10-21 18:08:47 +0530

    php test is done

---
+++ app/Http/Controllers/ApiController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Log;
+
+class ApiController extends Controller
+{
+    /**
+     * get data by calling api
+     * return @array
+     */
+    public function index()
+    {
+        /**
+         * Call the API
+         */
+        $response = Http::post('http://devapi.hidoc.co:8080//TrendingPastCases/PastCases?action=getTrendingCasesCP&userId=586&lastCount=0&searchKeyword=Cance', [
+            'action' => 'getTrendingCasesCP',
+            'userId' => 586,
+            'lastCount' => 0,
+            'searchKeyword' => 'Cance',
+        ]);
+
+        /**
+         * Parse the data
+         */
+        $data = $response->json();
+        // dd($data);
+
+        /**
+         * Check if 'data' exists in the response
+         */
+        if (isset($data) && is_array($data)) {
+            // dd($data);
+            /**
+             * Display the data
+             */
+            return view('api.index', ['data' => $data]);
+        } else {
+            /**
+             * Log the error response
+             */
+            Log::error('API Request Failed: ' . $response->body());
+
+            /**
+             * Handle invalid API response structure
+             */
+            return view('api.index', ['data' => []]); // Or handle it in another way
+        }
+
+    }
+}
---
+++ resources/views/api/index.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>API Data</title>
+</head>
+<body>
+    <h1>API Data</h1>
+    <ul>
+        <?php // dd($data['TotalCasesCP']);
+        ?>
+        @if (!empty($data['TotalCasesCP']))
+            @foreach($data['TotalCasesCP'] as $case)
+       
+                <li>
+                    <strong>Case Title:</strong> {{ $case['speciality'] }}<br>
+                    <strong>Admin Name:</strong> {{ $case['admin'] }}<br>
+                    <strong>Case Image:</strong> <img src="{{ $case['image'] }}" alt="Case Image"><br>
+                    <strong>Created Date:</strong> {{ $case['createdat'] }}<br><br>
+
+                </li>
+        
+            @endforeach
+            @else
+            <h4 style="color:red">Data not available.</h4>
+        @endif
+    </ul>
+</body>
+</html>
